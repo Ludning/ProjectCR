d3e613d351f279e666906a6abff19adcb80ee043
박문수 <b1372312369@gmail.com>
2024-06-17 12:38:19 +0900

    [Init] 프로젝트 초기화

---
+++ Assets/Script/UI/MVVM/ViewModel/ViewModelBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class ViewModelBase : MonoBehaviour
+{
+    public virtual void RegisterEventsOnEnable()
+    {
+    }
+    public virtual void UnRegisterEventsOnDisable()
+    {
+    }
+    
+    
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
