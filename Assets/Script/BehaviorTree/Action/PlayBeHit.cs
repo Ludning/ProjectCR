b610ca8752849a35707350b54584f75e251856d7
박문수 <b1372312369@gmail.com>
2024-06-26 18:02:43 +0900

    [Update] 비헤이비어 트리 수정

---
+++ Assets/Script/BehaviorTree/Action/PlayBeHit.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using BehaviorDesigner.Runtime.Tasks;
+using UnityEngine;
+
+[TaskCategory("CustomAction")]
+public class PlayBeHit : Action
+{
+    
+}
